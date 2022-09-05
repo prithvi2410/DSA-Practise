cbe7b1c25953cf4d92ceed8c6d2746f38e8d4ff2
Prithviraj S P <prithvi2410rj@gmail.com>
2022-09-05 16:45:49 +0530

    Bitwise Notes

--- 8 Math For DSA/1 BitWise Operator/Program.cs
+++ 8 Math For DSA/1 BitWise Operator/Program.cs
@@ -2,13 +2,14 @@
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("input is : "+IsOddOrEven(34));
-        Console.WriteLine("unique number is : "+GetUnique(new int[]{2,4,5,4,2}));
+        Console.WriteLine("input is : "+IsOddOrEven(35));
+        Console.WriteLine("unique number is : "+GetUnique(new int[]{2,2,4,4,5,4,2}));
         Console.ReadKey();
     }
 
     private static string IsOddOrEven(int number)
     {
+        Console.WriteLine(number&1);
         if( (number&1) == 1)
             return "odd";
 
@@ -18,10 +19,10 @@
     {
         int unique = 0;
         foreach(int num in arr)
-            {
-                //using XOR for each number in array finally gives us unique number in the array
-                unique^=num;
-            }
+        {
+            //Using XOR for each number in array finally gives us unique number in the array
+            unique^=num;
+        }
                 
         return unique;
     }
